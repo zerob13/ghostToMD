e2fe201e4963bd7ec6d962f25ff2334bda3d09f7
zerob13 <zerob13@gmail.com>
2014-07-10 15:10:05 +0800

    finish read title and markdown

--- ghostExport.go
+++ ghostExport.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+	db, err := sql.Open("sqlite3", "./blog.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	rows, err := db.Query("select title,markdown from posts")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+	for rows.Next() {
+		var title string
+		var markdown string
+		rows.Scan(&title, &markdown)
+		fmt.Println(title, markdown)
+	}
+	rows.Close()
+}
